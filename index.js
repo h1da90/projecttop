ba694876fd737b8395a7a02abbb72bbcd4333db9
h1da90 <lozailuna08@gmail.com>
2025-02-10 19:26:13 +0600

    artur lox chort

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const secondbutton = document.getElementById("secondbutton");
 const firstbutton = document.getElementById("firstbutton");
+const modal = document.getElementById("modal");
+const close = document.querySelector(".close");
 
 
 firstbutton.addEventListener("mousemove", () => {
@@ -15,3 +17,17 @@ secondbutton.addEventListener("mousemove", () => {
 secondbutton.addEventListener("mouseleave", () => {
     firstbutton.classList.remove("firstbutton")
 })
+
+firstbutton.addEventListener("click", () => {
+    modal.style.display = "block";
+});
+
+close.addEventListener("click", () => {
+    modal.style.display = "none";
+});
+
+window.addEventListener("click", (e) => {
+    if (e.target === modal) {
+        modal.style.display = "none";
+    }
+});
