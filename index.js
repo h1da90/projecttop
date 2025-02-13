4d1e7f5356a89accb014d3f6ee9ba5df6348dcca
h1da90 <lozailuna08@gmail.com>
2025-02-13 14:05:49 +0600

    animations

--- index.js
+++ index.js
@@ -32,21 +32,51 @@ window.addEventListener("click", (e) => {
 });
 
 document.getElementById("homeNaw").addEventListener("click", function () {
-    window.scrollTo({ top: 0, behavior: "smooth" });
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
+
+document.getElementById("aboutNaw").addEventListener("click", function () {
+  window.location.href = "YOUR_LINK_HERE";
+});
+
+document.getElementById("findNaw").addEventListener("click", function () {
+  const dropdown = document.getElementById("findDropdown");
+  dropdown.style.display = dropdown.style.display === "none" ? "block" : "none";
+});
+
+document.getElementById("shareNaw").addEventListener("click", function () {
+  document.querySelector(".allchoice").scrollIntoView({ behavior: "smooth" });
+});
+
+document.getElementById("promoteNaw").addEventListener("click", function () {
+  document.querySelector(".thanksFor").scrollIntoView({ behavior: "smooth" });
+});
+
+document.getElementById("headBtn").onclick = function (e) {
+  document.getElementById("headIn").value = "";
+};
+document.getElementById("firstbutton").addEventListener("click", function () {
+  window.location.href = "https://www.instagram.com/iloza._/";
+});
+document.getElementById("secondbutton").addEventListener("click", function () {
+  window.location.href = "https://www.youtube.com/watch?v=8e2CS0wPz9I";
+});
+document.getElementById("subbut").addEventListener("click", function () {
+  window.location.href =
+    "https://lalafo.kg/bishkek/doma-i-dachi/prodazha-domov";
+});
+document.getElementById("subbut2").addEventListener("click", function () {
+  window.location.href = "http://an.kg/kupit-dom/";
+});
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("visible");
+    }
   });
-  
-  document.getElementById("aboutNaw").addEventListener("click", function () {
-    window.location.href = "https://pikabu.ru/story/istoriya_pro_troikh_druzey_4712998";
-  });
-  
-  document.getElementById("findNaw").addEventListener("click", function () {
-    document.querySelector(".subscribe").scrollIntoView({ behavior: "smooth" });
-  });
-  
-  document.getElementById("shareNaw").addEventListener("click", function () {
-    document.querySelector(".allchoice").scrollIntoView({ behavior: "smooth" });
-  });
-  
-  document.getElementById("promoteNaw").addEventListener("click", function () {
-    document.querySelector(".thanksFor").scrollIntoView({ behavior: "smooth" });
-  });
+});
+
+document.querySelectorAll(".animated").forEach((el) => {
+  observer.observe(el);
+});
